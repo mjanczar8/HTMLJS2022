3908de998af47c0f285c6370e6acbfc110d67617
mjanczarNEIT <mpjanczar@email.neit.edu>
2022-12-09 14:55:40 -0500

    Cargame Done

--- Week 8/cargame.js
+++ Week 8/cargame.js
@@ -3,29 +3,33 @@ var ctx = canvas.getContext("2d");
 
 var timer = requestAnimationFrame(main);
 
-var start = 50;
-var finish = 750;
+var start = 58;
+var finish = 956;
 
 //car vars
 var carPos = 2;
 var startFuel = randomNumber( canvas.width, 600);
 var fuel = startFuel;
-var fuelBarWidth = 300;
-var speed = 5;
+var fuelBarWidth = 512;
+var speed = 4;
 var gameOver = true;
 var carWidth = 50;
 
 
 //start timer vars
 var seconds = 3;
-var fps = 60;
+var fps = 165;
 var frames = fps;
 
 //game sprites
 var carSprite = new Image();
+var finishLine = new Image();
+var backgroundRace = new Image();
 carSprite.src = "images/car.png";
+finishLine.src = "images/FinishLine.png";
+backgroundRace.src = "images/backgroundRace.png";
 
-carSprite.onload = function(){
+backgroundRace.onload = function(){
     main();
 }
 
@@ -52,13 +56,16 @@ function main(){
     if(gameOver){
         //main menu screen
         ctx.save();
+        drawBackground();
         ctx.fillStyle = "black";
-        ctx.font = "30px Arial";
+        ctx.font = "40px Trebuchet MS";
         ctx.textAlign = "center";
         ctx.fillText("Press [Space] to Start", canvas.width/2, canvas.height/2);
+        ctx.fillText("Welcome!", 310, 50);
         ctx.restore();
 
     }else{
+        drawBackground();
         if(!gameOver && seconds > 0){
             runStartTimer();
             drawStartTimer();
@@ -74,7 +81,8 @@ function main(){
         drawCar();
         drawFuelBar();
 
-        if(fuel <= 0 || carPos + carWidth > finish){
+        if(fuel <= 0 || carPos + carWidth > finish + 50){
+            fuel = 0;
             drawResults();
         }
     }
@@ -82,12 +90,16 @@ function main(){
     timer = requestAnimationFrame(main);
 }
 
+function drawBackground(){
+    ctx.drawImage( backgroundRace, 1, 1, 1024, 768);
+}
+
 function drawStartFinishLines(){
     //draw start line
     ctx.fillStyle = "black";   
-    ctx.fillRect( start, 50, 10, 500);
+    ctx.fillRect( start, 100, 1, 560);
     //draw finish
-    ctx.fillRect( finish, 50, 10 ,500);
+    ctx.drawImage( finishLine, finish, 102, 24, 557);
 
 }
 
@@ -95,19 +107,19 @@ function drawCar(){
     //draw car
     ctx.fillStyle = "red";
     // ctx.fillRect( carPos, canvas.height/2, 40, 20);
-    ctx.drawImage( carSprite, carPos, canvas.height/2, carWidth, 20)
+    ctx.drawImage( carSprite, carPos, canvas.height/2 - 16, carWidth, 20);
 }
 
 function drawFuelBar(){
     var currentBarWidth = fuelBarWidth * (fuel/startFuel);
     ctx.fillStyle = "black";
-    ctx.fillRect( start, 30, fuelBarWidth, 10);
-    ctx.font = "25px Arial";
-    ctx.fillText("Fuel", start, 25);
+    ctx.fillRect( start, 38, fuelBarWidth, 10);
+    ctx.font = "20px Trebuchet MS";
+    ctx.fillText("Fuel", start, 34);
 
     if(fuel > 0){
-        ctx.fillStyle = "green"
-        ctx.fillRect( start, 30, currentBarWidth, 10);
+        ctx.fillStyle = "green";
+        ctx.fillRect( start, 38, currentBarWidth, 10);
     }
 
 }
@@ -116,19 +128,19 @@ function drawResults(){
     if(carPos + carWidth > finish){
         ctx.save();
         ctx.fillStyle = "black";
-        ctx.font = "25px Arial";
+        ctx.font = "40px Trebuchet MS";
         ctx.textAlign = "center";
         ctx.fillText("You made it to the finish... You win!!", canvas.width/2, canvas.height/2);
-        ctx.font = "15px Arial"
+        ctx.font = "30px Trebuchet MS"
         ctx.fillText("Press any key to restart.", canvas.width/2, canvas.height/2 + 30)
         ctx.restore();
     }else{
         ctx.save();
         ctx.fillStyle = "black";
-        ctx.font = "25px Arial";
+        ctx.font = "40px Trebuchet MS";
         ctx.textAlign = "center";
         ctx.fillText("You ran out of fuel... You lose!!", canvas.width/2, canvas.height/2)
-        ctx.font = "15px Arial"
+        ctx.font = "30px Trebuchet MS"
         ctx.fillText("Press any key to restart.", canvas.width/2, canvas.height/2 + 30)
         ctx.restore();
     }
@@ -154,14 +166,14 @@ function drawStartTimer(){
     if(seconds > 0){
         ctx.save();
         ctx.fillStyle = "black";
-        ctx.font = "30px Arial";
+        ctx.font = "70px Trebuchet MS";
         ctx.textAlign = "center";
         ctx.fillText(seconds, canvas.width/2, canvas.height/2)
         ctx.restore();
     }else{
         ctx.save();
         ctx.fillStyle = "black";
-        ctx.font = "30px Arial";
+        ctx.font = "70px Trebuchet MS";
         ctx.textAlign = "center";
         ctx.fillText("GO!", canvas.width/2, canvas.height/2)
         ctx.restore();
